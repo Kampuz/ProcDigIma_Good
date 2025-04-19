70dabb345bbcb835a84807c4f81e637a71703275
Miguel de Campos Rodrigues Moret <miguel.moret@unesp.br>
2025-04-19 18:00:46 -0300

    feat: added a components in form1
    - Main menu with all operations required in the assignment
    
    - 4 images fields
    
    - Labels and text fields for x, y, R, G, B, H, S, V
    
    - Button to move result image to main image
    
    - Open and Save dialog

--- New Code/unit1.pas
+++ New Code/unit1.pas
@@ -5,10 +5,67 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls,
+  ExtCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    EditX: TEdit;
+    EditR: TEdit;
+    EditG: TEdit;
+    EditB: TEdit;
+    EditY: TEdit;
+    EditH: TEdit;
+    EditS: TEdit;
+    EditV: TEdit;
+    ImagemOriginal: TImage;
+    ImagemResultado: TImage;
+    CanalVermelho: TImage;
+    CanalVerde: TImage;
+    CanalAzul: TImage;
+    LabelX: TLabel;
+    LabelY: TLabel;
+    LabelR: TLabel;
+    LabelG: TLabel;
+    LabelB: TLabel;
+    LabelH: TLabel;
+    LabelS: TLabel;
+    LabelV: TLabel;
+    MenuItemEqualizacao: TMenuItem;
+    MenuItemBinarizacao: TMenuItem;
+    MenuItemCompressao: TMenuItem;
+    MenuItemLaplaciano: TMenuItem;
+    MenuItemSobel: TMenuItem;
+    MenuItemMediana: TMenuItem;
+    MenuItemMedia: TMenuItem;
+    MenuItemRuidos: TMenuItem;
+    MenuItemHSVparaRGB: TMenuItem;
+    MenuItemRGBparaHSV: TMenuItem;
+    MenuItemRestaurar: TMenuItem;
+    MenuItemSeparar: TMenuItem;
+    MenuItemNegativo: TMenuItem;
+    MenuItemCinza: TMenuItem;
+    MenuItemOperacao: TMenuItem;
+    MenuItemFiltros: TMenuItem;
+    MenuItemCanais: TMenuItem;
+    MenuItemConversores: TMenuItem;
+    MoverImagem: TButton;
+    MainMenu: TMainMenu;
+    MenuItemImagem: TMenuItem;
+    MenuItemAbrir: TMenuItem;
+    MenuItemSalvar: TMenuItem;
+    MenuItemFechar: TMenuItem;
+    OpenDialog: TOpenDialog;
+    SaveDialog: TSaveDialog;
+    Separator1: TMenuItem;
+    procedure FormCreate(Sender: TObject);
+    procedure ImagemOriginalClick(Sender: TObject);
+    procedure LabelSClick(Sender: TObject);
+    procedure MoverImagemClick(Sender: TObject);
+    procedure MenuItemImagemClick(Sender: TObject);
   private
 
   public
@@ -21,6 +78,33 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.MenuItemImagemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MoverImagemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.ImagemOriginalClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.LabelSClick(Sender: TObject);
+begin
+
+end;
 
 end.
 
